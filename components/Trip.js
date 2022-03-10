923a10532dd57bd901fa444401d0d7f3301089d8
suijt <st.zimi1@gmail.com>
2022-03-10 17:24:14 +0545

    video bg fixed

--- components/Trip.js
+++ components/Trip.js
@@ -4,7 +4,7 @@ const Trip = () => {
   return (
     <div className=" ">
       <video loop autoPlay>
-        <source src="../assets/videos/hero-bg.mp4" type="video/mp4" />
+        <source src="video/hero-bg.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
